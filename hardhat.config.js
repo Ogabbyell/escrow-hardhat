0840e2cd9fab459c786688fdbc73f7ef9dfb2ecd
gabriel oni <dlastdj10@gmail.com>
2023-05-22 10:03:45 +0100

    First Commit

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require('@nomicfoundation/hardhat-toolbox');
+
+module.exports = {
+  solidity: "0.8.17",
+  paths: {
+    artifacts: "./app/src/artifacts",
+  }
+};
+
+
+
+// Account #19: 0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199 (10000 ETH)
+// Private Key: 0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e
